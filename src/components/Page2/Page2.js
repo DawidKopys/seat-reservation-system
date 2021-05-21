d8696fada0d5b79f8c19aa713900b1631be9e084
Dewke <dewke17@gmail.com>
2021-05-21 22:04:02 +0200

    Finish Page2 layout

--- src/components/Page2/Page2.js
+++ src/components/Page2/Page2.js
@@ -6,24 +6,46 @@ import { Button } from 'antd';
 
 const Page2 = (props) => {
   return (
-    <div className='seats'>
-      {seats.map((seat) => {
-        const {
-          id,
-          cords: { x, y },
-          reserved,
-        } = seat;
-        const reservedClass = `${reserved ? 'seats__seat-btn--reserved' : ''}`;
+    <div className='seats-choice-container'>
+      <div className='seats'>
+        {seats.map((seat) => {
+          const {
+            id,
+            cords: { x, y },
+            reserved,
+          } = seat;
+          const reservedClass = `${reserved ? 'seat-btn--reserved' : ''}`;
 
-        return (
-          <button
-            className={`btn seats__seat-btn ${reservedClass}`}
-            key={id}
-            disabled={reserved}
-            style={{ gridRow: x + 1, gridColumn: y + 1 }}
-          ></button>
-        );
-      })}
+          return (
+            <Button
+              htmlType='button'
+              className={`seat-btn seats__seat-btn ${reservedClass}`}
+              key={id}
+              disabled={reserved}
+              style={{ gridRow: x + 1, gridColumn: y + 1 }}
+            >
+              {' '}
+            </Button>
+          );
+        })}
+      </div>
+      <div className='legend seats-choice-container__legend'>
+        <div className='legend__item'>
+          <div className={`seat-btn legend__seat-btn`} />
+          Miejsca dostępne
+        </div>
+        <div className='legend__item'>
+          <div className={`seat-btn legend__seat-btn seat-btn--reserved`} />
+          Miejsca zarezerwowane
+        </div>
+        <div className='legend__item'>
+          <div className={`seat-btn legend__seat-btn seat-btn--chosen`} />
+          Twój wybór
+        </div>
+        <Button htmlType='button' className='legend__confirmation-btn'>
+          Rezerwuj
+        </Button>
+      </div>
     </div>
   );
 };
